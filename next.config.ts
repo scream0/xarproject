7a897dc23f163c63a04469d02c8da47bcc24de0c
scream0 <cimohcihui@gmail.com>
2026-07-21 00:03:09 +0700

    update ke 0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    unoptimized: true, // Wajib jika pakai Firebase Hosting
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://snap-assets.sandbox.midtrans.com https://app.sandbox.midtrans.com; connect-src 'self' https://api.sandbox.midtrans.com; frame-src 'self' https://app.sandbox.midtrans.com;`,
+          },
+        ],
+      },
+    ];
   },
 };
 
