4b40262af5a5d47ccd1611b60e9659bc6c536bfe
scream0 <cimohcihui@gmail.com>
2026-07-21 18:40:36 +0700

    fixed csp

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,31 @@
 /** @type {import('next').NextConfig} */
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://snap-assets.sandbox.midtrans.com https://api.sandbox.midtrans.com https://app.sandbox.midtrans.com https://pay.google.com https://gwk.gopayapi.com https://www.googletagmanager.com https://o.alicdn.com https://g.alicdn.com;
+  connect-src 'self' https://api.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://gwk.gopayapi.com;
+  frame-src 'self' https://app.sandbox.midtrans.com https://pay.google.com https://gwk.gopayapi.com;
+  img-src 'self' data: https://*.midtrans.com https://*.alicdn.com;
+  style-src 'self' 'unsafe-inline';
+`;
+
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  /* async headers() {
+  async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://snap-assets.sandbox.midtrans.com https://app.sandbox.midtrans.com; connect-src 'self' https://api.sandbox.midtrans.com; frame-src 'self' https://app.sandbox.midtrans.com;`,
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\s{2,}/g, " ").trim(),
           },
-        ],s
+        ],
       },
     ];
-  },*/
+  },
 };
 
 export default nextConfig;
